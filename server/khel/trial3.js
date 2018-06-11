a3ba68d0fd891392493c0d04d3e72946a15d29c7
GGKTECH\ujwala.garlapati <ujwala.garlapati@ggktech.com>
2018-06-11 20:36:19 +0530

    commit on 11/06/18

--- server/khel/trial3.js
+++ server/khel/trial3.js
@@ -3,54 +3,83 @@ var _ = require('lodash');
 var Promise = require('bluebird')
 
 var Category_Info;
- Category_Details().then((a)=>{
-   Category_Info = a;
-    Match_Schedule();
- })
+var Team_Id = 0;
+var player_team_details_insert_query = "insert into player_team_details values";
+var team_details_insert_query = "insert into team_details values"
+Category_Details().then(() => { // returns game categories from game_category_details table)
+    Categorize();
+})
 
-function Match_Schedule() {
+function Categorize() { // Groups players with common interests together
     //console.log('cat info',Category_Info)
-    knex('group_details')
+    console.log("2")
+   return knex('group_details')  // Select unique group ids
         .select('group_id')
         .then(function (group_ids) {
-            var groups = _.map(group_ids, 'group_id');
-            Promise.map(groups, function (group) {
+            let groups = _.map(group_ids, 'group_id');
+          return Promise.map(groups, function (group) { // Get player details, one group at a time
                 return Load_Data_From_Database(group)
                     .then(function (group_members) {
                         return _.groupBy(group_members, 'category_id');
                     })
-                    .then(function (sorted_by_categories) {
+                    .then(function (sorted_by_categories) { // categorize according to group, category
                         var categories = Object.values(sorted_by_categories)
                         return _.map(categories, function (category) {
                             return _.groupBy(category, 'gender')
                         })
                     })
-                    .then(function (sorted_by_gender) {
+                    .then(function (sorted_by_gender) { //categorize according to group, category, gender
+  // change to promise.each
+  //put return statements to all promises and functions
                         _.each(sorted_by_gender, (gender_Categories) => {
-                            _.each(gender_Categories, function (gender) {
-                                Make_Matches(gender)
+                            _.each(gender_Categories, function (players) {                               
+                                Make_Matches(players)
                             })
+
                         })
+                        console.log('ujwala');
                     })
             })
         })
-        
 }
+/**
+ * 
+ * @param {*} players 
+ */
+function Make_Matches(players) {
+    console.log("'''''''''''''''''''''''''''''''''''''''''''''''")
+    console.log(players)
 
-function Make_Matches(gender) {
-
-    let player_count = gender.length;
-    if (player_count > 1) {
-        var index = 0;
-var Category_ID =  gender[0].category_id;
-var Category_Name = ((Category_Info.filter(function( ci ) {
-  return ci.category_id == Category_ID;
-})).map(a =>  a.category_name)).toString();
-console.log("Name", Category_Name)
-
-        var query = "insert into match_details values(";
-
-    }
+    let index = 0;
+    let player_count = players.length;
+    let Category_ID = players[0].category_id;
+    let Team_Capacity = (_.pick((_.find(Category_Info, { category_id: Category_ID })), 'no_of_players'))['no_of_players']
+   console.log("team Capacity", Team_Capacity)
+   if(player_count < 2*Team_Capacity)
+   {
+       return 0;
+   }
+    let No_Of_Initial_Matches = player_count / (2 * Team_Capacity)
+    console.log("QQQQQQQQQQQQ",No_Of_Initial_Matches)
+    let Chunks_Of_Teams = _.chunk(players, Team_Capacity)
+    console.log(Chunks_Of_Teams)
+    Promise.each(Chunks_Of_Teams, function (team) {
+        console.log("1111111")
+        console.log("AAAAAAAAA",team)
+        return Fill_Team_Details(team, Team_Capacity)
+    })
+        .then(function (a) {
+            team_details_insert_query = _.replace(team_details_insert_query, ',', '')
+            //console.log(team_details_insert_query)
+            return knex.raw(team_details_insert_query)
+        })
+        .then(function () {
+            player_team_details_insert_query = _.replace(player_team_details_insert_query, ',', '')
+            //console.log(player_team_details_insert_query)
+            return knex.raw(player_team_details_insert_query)
+        })
+    var query = "insert into match_details values(";
+ 
 }
 
 function Load_Data_From_Database(group) {
@@ -61,11 +90,31 @@ function Load_Data_From_Database(group) {
 }
 
 function Category_Details() {
+    //console.log("1")
     return knex.from('game_category_details')
-        .select().then(function(n)
-        {
-           Category_Info =  n;
-           return Category_Info
+        .select().then(function (n) {
+            Category_Info = n;
+            //return Category_Info
         })
-        
+
+}
+
+function Fill_Team_Details(team_players, Team_Capacity) {
+    console.log("4",Team_Capacity)
+    //    console.log(team_players+"      "+Team_Capacity)     
+    if (team_players.length != Team_Capacity) {
+        return 0;
+    }
+    Team_Id++;
+    //console.log("team", Team_Id)
+    var player_ids = team_players.map(a => a.player_id)
+   // console.log("player",player_ids)
+    team_details_insert_query = team_details_insert_query.concat(",(" + Team_Id + ")");
+    return Promise.each(player_ids, function (Player_ID) {
+     //   console.log(player_ids, "              ", Player_ID)
+    //    console.log("4.1")
+    return player_team_details_insert_query = player_team_details_insert_query.concat(",", "(" + Team_Id + "," + Player_ID + ")");
+   
+ })
+
 }
